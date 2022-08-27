ccdcf662d7ef8d369ce66c27fd6f918a41a3a424
llowq <96781765+kenDevXD@users.noreply.github.com>
2022-08-27 21:39:33 +0700

    Add files via upload

--- menu.sh
+++ menu.sh
@@ -78,13 +78,10 @@ else
 Exp=$(curl -sS https://raw.githubusercontent.com/kenDevXD/src/main/ip | grep $MYIP | awk '{print $3}')
 fi
 clear
-echo -e "${CYAN}╒━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╕\033[0m${NC}"
-echo -e " \E[0;41;36m     ⇱ AutoScript By Ridwan Project ⇲       \E[0m"
-echo -e "${CYAN}╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛\033[0m${NC}"
-echo -e "\e[36m[ SSH WebSocket : ON ]     [ XRAY : ON ] \033[0m"
-echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
-echo -e " \E[0;41;36m                 INFO SERVER                \E[0m"
-echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
+echo -e "\e[36m╒━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╕\033[0m"
+echo -e " \E[44;1;39m        ⇱ AutoScript By KennXV ⇲        \E[0m"
+echo -e " \E[44;1;39m            ⇱ Informasi VPS SERVER ⇲            \E[0m"
+echo -e "\e[36m╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛\033[0m"
 uphours=`uptime -p | awk '{print $2,$3}' | cut -d , -f1`
 upminutes=`uptime -p | awk '{print $4,$5}' | cut -d , -f1`
 uptimecek=`uptime -p | awk '{print $6,$7}' | cut -d , -f1`
@@ -100,45 +97,26 @@ fi
 echo -e "Use Core        :  $rekk"
 echo -e "Current Domain  :  $(cat /etc/$bec/domain)"
 echo -e "IP-VPS          :  $IPVPS"
-echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
-echo -e " \E[0;41;36m                 SSH MENU                   \E[0m"
-echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
-echo -e " [\033[1;36m01\033[0m]  Add User SSH
- [\033[1;36m02\033[0m]  SSH WS Enable
- [\033[1;36m03\033[0m]  Cek User SSH
- [\033[1;36m04\033[0m]  Del User SSH
- [\033[1;36m05\033[0m]  Renew SSH
- [\033[1;36m06\033[0m]  Member
-\e[36m╒════════════════════════════════════════════╕\033[0m"
-echo -e " \E[0;41;36m                 XRAY MENU                  \E[0m"
-echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m
- [\033[1;36m07\033[0m]  Add Vmess Account
- [\033[1;36m08\033[0m]  Add Vless Account
- [\033[1;36m09\033[0m]  Addd Trojan Account
- [\033[1;36m10\033[0m]  Add Sodosok Account
- [\033[1;36m11\033[0m]  Cek User Xray
- [\033[1;36m12\033[0m]  Del User Xray
- [\033[1;36m13\033[0m]  Renew User Xray"
-echo -e  "\e[36m╒════════════════════════════════════════════╕\033[0m"
-echo -e " \E[0;41;36m               Settings MENU                \E[0m"
-echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m
- [\033[1;36m14\033[0m]  Add-host
- [\033[1;36m15\033[0m]  Gen SSL 
- [\033[1;36m16\033[0m]  Running Vps 
- [\033[1;36m17\033[0m]  Speedtest
- [\033[1;36m18\033[0m]  Xol Panel Bot
- [\033[1;36m00\033[0m]  BETA VERSION DRG
- "
-if [[ $(cat /opt/.ver) = $serverV ]] > /dev/null 2>&1; then
-echo -ne
-else
-echo -e "[\033[1;32m999\033[0m] • \033[0;31mUpdate Available ! Go choice 69 to update\033[0m"
-echo ""
-fi
-echo -e "\033[1;37mPress [ Ctrl+C ] • To-Exit-Script\033[0m"
-echo ""
-#echo -e "\e[36m╘════════════════════════════════════════════════════╛\033[0m"
-echo -e "\e[36m╒═════════════════════════════════════════════╕\033[0m"
+echo -e "\e[36m╒━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╕\033[0m"
+echo -e " \E[44;1;39m             ⇱ Status Layanan Service ⇲         \E[0m"
+echo -e "\e[36m╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛\033[0m"
+
+ echo -e " [ SSH WebSocket : ON ]     [ XRAY : ON ]      [ NGINX : ON ]"
+
+echo -e "\e[36m╒━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╕\033[0m"
+echo -e " \E[44;1;39m               ⇱ Status Layanan VPS ⇲           \E[0m"
+echo -e "\e[36m╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛\033[0m"
+
+echo -e " [\033[1;36m01\033[0m] • SSH-WS Manager       [\033[1;36m08\033[0m] • Info Pembuat
+ [\033[1;36m02\033[0m] • Vmess Manager        [\033[1;36m09\033[0m] • Renew Cert
+ [\033[1;36m03\033[0m] • Vless Manager        [\033[1;36m10\033[0m] • Speedtest
+ [\033[1;36m04\033[0m] • Trojan Manager       [\033[1;36m11\033[0m] • Coming Soon
+ [\033[1;36m05\033[0m] • SS Manager           [\033[1;36m12\033[0m] • Cek Bandwith
+ [\033[1;36m06\033[0m] • Add Private Domain   [\033[1;36m13\033[0m] • BACKUP
+ [\033[1;36m07\033[0m] • Running All Service  [\033[1;36m14\033[0m] • RESTORE DATA"
+
+echo -e "\e[36m╒━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╕\033[0m"
+
 if [[ $(cat /opt/.ver) = $serverV ]] > /dev/null 2>&1; then
 echo -e "Version       :\033[1;36m $(cat /opt/.ver) Latest Version\e[0m"
 echo -e "Client Name   : $Name"
@@ -148,53 +126,47 @@ else
 rm /dev/.permiss > /dev/null 2>&1
 touch /home/needupdate > /dev/null 2>&1
 echo -e "\033[0;33mVersion : $(cat /opt/.ver) Update available to $serverV\e[0m"
-echo -e "\e[36m╒═════════════════════════════════════════════╕\033[0m"
-echo ""
-echo -e "[ \033[0;31mChangelog\033[0m ]"
-curl -sS https://raw.githubusercontent.com/kenDevXD/src/main/clgshow
-echo -e "
-"
 fi
-echo -e "\e[36m╘═════════════════════════════════════════════╛\033[0m"
-echo
+echo -e "\e[36m╘━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╛\033[0m"
+echo -e ""
 echo -ne "Select menu : "; read x
 if [[ $(cat /opt/.ver) = $serverV ]] > /dev/null 2>&1; then
     if [[ $x -eq 1 ]]; then
-       usernew
+       ssh-menu
     elif [[ $x -eq 2 ]]; then
-       sshws
+       v2ray-menu
     elif [[ $x -eq 3 ]]; then
-       cek
+       trojan-menu
     elif [[ $x -eq 4 ]]; then
-       hapus
+       ssgrpc-menu
     elif [[ $x -eq 5 ]]; then
-       renew
-    elif [[ $x -eq 6 ]]; then
-       member
-    elif [[ $x -eq 7 ]]; then
-       add-ws
-    elif [[ $x -eq 8 ]]; then
-       add-vless
-    elif [[ $x -eq 9 ]]; then
-       add-tr
-    elif [[ $x -eq 10 ]]; then
-       add-ssws
-    elif [[ $x -eq 11 ]]; then
-       cek-ws
-    elif [[ $x -eq 12 ]]; then
-       del-user
-    elif [[ $x -eq 13 ]]; then
-       renew-ws
-    elif [[ $x -eq 14 ]]; then
        add-host
-    elif [[ $x -eq 15 ]]; then
-       crtv2ray
-    elif [[ $x -eq 16 ]]; then
+    elif [[ $x -eq 6 ]]; then
        running
-    elif [[ $x -eq 17 ]]; then
+    elif [[ $x -eq 7 ]]; then
+       about
+    elif [[ $x -eq 8 ]]; then
+       crtv2ray
+    elif [[ $x -eq 9 ]]; then
        speedtest
+    elif [[ $x -eq 10 ]]; then
+       about
+    elif [[ $x -eq 11 ]]; then
+       about
+    elif [[ $x -eq 12 ]]; then
+       about
+    elif [[ $x -eq 13 ]]; then
+       about
+    elif [[ $x -eq 14 ]]; then
+       about
+    elif [[ $x -eq 15 ]]; then
+       about
+    elif [[ $x -eq 16 ]]; then
+       about
+    elif [[ $x -eq 17 ]]; then
+       about
      elif [[ $x -eq 18 ]]; then
-       xolpanel
+       about
     else
        menu
     fi
@@ -204,41 +176,41 @@ else
        screen -S upds update-script
        menu
     elif [[ $x -eq 1 ]]; then
-       usernew
+       ssh-menu
     elif [[ $x -eq 2 ]]; then
-       sshws
+       v2ray-menu
     elif [[ $x -eq 3 ]]; then
-       cek
+       trojan-menu
     elif [[ $x -eq 4 ]]; then
-       hapus
+       ssgrpc-menu
     elif [[ $x -eq 5 ]]; then
-       renew
-    elif [[ $x -eq 6 ]]; then
-       member
-    elif [[ $x -eq 7 ]]; then
-       add-ws
-    elif [[ $x -eq 8 ]]; then
-       add-vless
-    elif [[ $x -eq 9 ]]; then
-       add-tr
-    elif [[ $x -eq 10 ]]; then
-       add-ssws
-    elif [[ $x -eq 11 ]]; then
-       cek-ws
-    elif [[ $x -eq 12 ]]; then
-       del-user
-    elif [[ $x -eq 13 ]]; then
-       renew-ws
-    elif [[ $x -eq 14 ]]; then
        add-host
-    elif [[ $x -eq 15 ]]; then
-       crtv2ray
-    elif [[ $x -eq 16 ]]; then
+    elif [[ $x -eq 6 ]]; then
        running
-    elif [[ $x -eq 17 ]]; then
+    elif [[ $x -eq 7 ]]; then
+       about
+    elif [[ $x -eq 8 ]]; then
+       crtv2ray
+    elif [[ $x -eq 9 ]]; then
        speedtest
+    elif [[ $x -eq 10 ]]; then
+       about
+    elif [[ $x -eq 11 ]]; then
+       about
+    elif [[ $x -eq 12 ]]; then
+       about
+    elif [[ $x -eq 13 ]]; then
+       about
+    elif [[ $x -eq 14 ]]; then
+       about
+    elif [[ $x -eq 15 ]]; then
+       about
+    elif [[ $x -eq 16 ]]; then
+       about
+    elif [[ $x -eq 17 ]]; then
+       about
      elif [[ $x -eq 18 ]]; then
-       xolpanel
+       about
     else
        menu
     fi
